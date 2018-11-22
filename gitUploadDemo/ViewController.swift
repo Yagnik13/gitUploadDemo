9a0d359daf03a1cb13ef1a0bf13d2adf96138e1f
Yagnik Suthar <yagnik.suthar@ecosmob.com>
2018-11-22 09:34:22 +0530

    Initial Commit

---
+++ gitUploadDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  gitUploadDemo
+//
+//  Created by Yagnik Suthar on 22/11/18.
+//  Copyright © 2018 Yagnik Suthar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
